fff7fbc8b7e93ceef4f44ca4c83a12b3a8e05adf
diego <diegofonterosa@gmail.com>
2025-03-14 18:54:24 +0100

    Cambios2 14/03

--- Clase4.py
+++ Clase4.py
@@ -27,8 +27,27 @@
 #Ejercicio 3: Manipulación avanzada de cadenas
 #Crea una cadena larga que contenga espacios en blanco al inicio, final, y en medio.
 # Realiza varias operaciones encadenadas como eliminar
-# espacios extremos, covertir a mayúsculas, y dividir la cadena en varias subcadenas usando un separador específico.
+# espacios extremos, covertir to_do a mayúsculas, y dividir la cadena en varias subcadenas usando un separador específico.
 
-cadena = " Esto es un ejemplo con huecos delante y detrás "
-nueva_cadena = cadena.strip().upper().split()
-print(nueva_cadena)
+#cadena = " Esto es un ejemplo con huecos delante y detrás "
+#nueva_cadena = cadena.strip().upper().split()
+#print(nueva_cadena)
+
+#Ejercicio 4: Índices y subcadenas
+#Define una cadena extensa (mínimo 50 caracteres).
+# Obtén varias subcadenas usando la indexación por rangos (slicing) y genera una nueva cadena combinando estas subcadenas en orden inverso.
+# Imprime la nueva cadena resultante.
+
+#cadena_extensa = "Python en un lenguaje que me encanta y que me da mucho trabajo."
+#subcadena = cadena_extensa[0:6]+ "" + cadena_extensa[11:20] + "" + cadena_extensa [-9:]
+#resultado = subcadena[::-1]
+#print(resultado)
+
+#Ejercicio 5: Formato y conversión numérica
+#Define variables numéricas (entero, flotante, complejo).
+# Crea una cadena con formato avanzado (f-strings) que muestre estos números con precisión definida (dos decimales, notación científica, etc.).
+# Evita concatenar directamente números y texto.
+
+entero, flotante, complejo = 12, 367.7864574, 5+3j
+formato = f"Entero: {entero}, Flotante: {flotante:.2f}, Notación cintífica: {flotante:.2e} , Complejo: {complejo}"
+print(formato)
