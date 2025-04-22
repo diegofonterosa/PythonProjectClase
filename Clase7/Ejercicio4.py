1a82a057df3c788377ba44695d152b57f5f74e93
diego <diegofonterosa@gmail.com>
2025-04-22 13:00:56 +0200

    Cambios 10/04

--- Clase7/Ejercicio4.py
+++ Clase7/Ejercicio4.py
@@ -4,5 +4,9 @@ numero = int(input("Introduce un número entero: "))
 
 if numero % 3 == 0 and numero % 5 == 0:
     print(f"{numero} es múltiplo de 3 y de 5")
-elif numero % 3 == and numero % 5 != 0:
+elif numero % 3 == 0:
     print(f"{numero} es múltiplo de 3")
+elif numero % 5 == 0:
+    print(f"{numero} es múltiplo de 5")
+else:
+    print(f"{numero} no es múltiplo ni de 3 ni de 5")
